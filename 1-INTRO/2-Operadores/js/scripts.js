b80a7136c798496042b159d66c9a443beb0cc618
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-07-10 17:01:37 -0300

    coloquei pra rodar meu script funcionou saiu os resultados da minhas contas.

---
+++ 1-INTRO/2-Operadores/js/scripts.js
@@ -0,0 +1,12 @@
+// 1-Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2-Ops.aritmeticas
+console.log(2 + 4);
+console.log(10 -5);
+console.log(5 * 4);
+console.log(10 /2);
+
+console.log(5  +(4 * 2))
