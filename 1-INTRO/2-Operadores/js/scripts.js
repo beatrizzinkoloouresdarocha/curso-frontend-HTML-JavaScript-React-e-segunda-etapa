6ff746b570ff247cb92b5118f3fa731a0a6b7a7c
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-07-10 17:35:51 -0300

    crei meu scripts   testei funcionou quando coloquei pra rodar .

--- 1-INTRO/2-Operadores/js/scripts.js
+++ 1-INTRO/2-Operadores/js/scripts.js
@@ -9,4 +9,25 @@ console.log(10 -5);
 console.log(5 * 4);
 console.log(10 /2);
 
-console.log(5  +(4 * 2))
+console.log(5  +(4 * 2));
+
+// 3-Especial number //
+console.log(typeof Infinity)
+
+console.log(typeof - Infinity)
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+//4-Strings //
+console.log("um texto");
+console.log('mais um texto');
+console.log('13');
+
+console.log(typeof"um texto");
+console.log(typeof'mais um texto');
+
+// 5-Simbolos especiais e3m strings //
+console.log("testando a/n quebra linha")
+
