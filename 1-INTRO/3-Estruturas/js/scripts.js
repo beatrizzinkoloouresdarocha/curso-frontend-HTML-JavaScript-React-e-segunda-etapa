ab64c555fa287c2b731af63d88825bf5d6f648e6
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-07-11 17:18:38 -0300

    estou conseguindo fazer todos os testes de javascripts e quando coloco pra rodar funciona sai o resuldo do que eu preciso .

--- 1-INTRO/3-Estruturas/js/scripts.js
+++ 1-INTRO/3-Estruturas/js/scripts.js
@@ -2,4 +2,27 @@
 let nome = "Beatriz";
 
 console.log(nome);
-console.log("Beatriz");
+
+nome= "Beatriz Zinko"
+
+console.log(nome);
+
+const idade=22;
+
+console.log(idade);
+
+//idade =32;
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// Mais sobre variáveis //
+// let 2teste ='invlido'//
+//let @2teste ="invalido"//
+
+let a=10 ,
+b=20,
+c=30;
+
+console.log(a,b,c);
