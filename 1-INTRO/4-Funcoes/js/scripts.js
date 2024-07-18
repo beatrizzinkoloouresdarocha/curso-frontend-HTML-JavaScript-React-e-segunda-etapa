64139880956a2c43886d12f350079f65c1e91101
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-07-18 15:40:40 -0300

    comecei agora este : // 2-return //.

--- 1-INTRO/4-Funcoes/js/scripts.js
+++ 1-INTRO/4-Funcoes/js/scripts.js
@@ -1,1 +1,21 @@
 // 1-criando uma função //
+function minhaFuncao(){
+    console.log("testando");
+}
+
+minhaFuncao();
+minhaFuncao();
+
+const minhaFuncaoEmvariavel= function() {
+    console.log("função variavel")
+}
+minhaFuncaoEmvariavel();
+
+function funcaoComParametro(txt) {
+    console.log(`Imprimindo: ${txt}`);
+}
+
+funcaoComParametro("imprimindo alguma coisa");
+funcaoComParametro("primeira função");
+
+// 2-return //
