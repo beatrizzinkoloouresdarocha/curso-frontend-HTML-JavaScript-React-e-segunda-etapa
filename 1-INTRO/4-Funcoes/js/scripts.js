326f3cb371d3f215a98120e5e805ef7ed65c37c1
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-07-18 15:52:47 -0300

    finalizei o exercicio // 2-return //.coloquei pra rodar funcionou.

--- 1-INTRO/4-Funcoes/js/scripts.js
+++ 1-INTRO/4-Funcoes/js/scripts.js
@@ -19,3 +19,17 @@ funcaoComParametro("imprimindo alguma coisa");
 funcaoComParametro("primeira função");
 
 // 2-return //
+const a =10;
+const b =20;
+const c =30;
+const d =40;
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+const resultado =soma(a,b);
+
+console.log(resultado);
+
+console.log(soma(c,d));
