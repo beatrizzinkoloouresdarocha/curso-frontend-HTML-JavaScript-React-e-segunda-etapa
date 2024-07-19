a5abd19565fa52d058632117f5c9f654b3f7b661
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-07-19 10:32:40 -0300

    finalizei o 1,2,3  testei e funvcionou quando coloquei pra rodar .

--- 1-INTRO/4-Funcoes/ARRAYS-E-OBJS/js/scripts.js
+++ 1-INTRO/4-Funcoes/ARRAYS-E-OBJS/js/scripts.js
@@ -4,3 +4,28 @@ const lista= [1,2,3,4,5];
 console.log(lista);
 
 console.log(typeof lista);
+
+const itens= ["Beatriz",true,2,4,12,[]]
+
+console.log(itens);
+
+// 2- mais sobre arrays //
+const arr =["a","b","c","d","d"]
+
+console.log(arr["0"]);
+
+console.log(arr["2"]);
+
+// 3-propriedades //
+
+const numbers =[5,3,4]
+
+console.log(numbers.length);
+
+console.log(numbers["length"])
+
+const myName = "Beatriz";
+
+console.log(myName.length);
+
+// 4-métodos //
